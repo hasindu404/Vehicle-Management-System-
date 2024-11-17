729322994fbc3539161a93595da37753d9febbd7
hasindu404 <prabhash11576@usci.ruh.ac.lk>
2024-11-17 12:58:38 +0530

    Update UserController.java

--- VSM-System/src/main/java/VSM_System/Controller/UserController.java
+++ VSM-System/src/main/java/VSM_System/Controller/UserController.java
@@ -1,5 +1,21 @@
 package VSM_System.Controller;
 
+import VSM_System.dto.UserDTO;
+import VSM_System.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
 
 }
